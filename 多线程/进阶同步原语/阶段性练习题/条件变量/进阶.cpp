d97d6e2c7f4b6e70e6d80bd6c28ad94cbac4f6c7
Jiaqi-Gu-DCT <y33782579@gmail.com>
2026-07-12 12:37:28 +0800

    2026-07-12

--- 多线程/进阶同步原语/阶段性练习题/条件变量/进阶.cpp
+++ 多线程/进阶同步原语/阶段性练习题/条件变量/进阶.cpp
@@ -5,50 +5,50 @@
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
-#include <vector>
-#include <queue>
 #include <thread>
+#include <queue>
+#include <vector>
 using namespace std;
 
 class S_queue{
 private:
     queue<int> q;
-    mutex mtx;
     int capacity;
+    mutex mtx;
     condition_variable not_full, not_empty;
-    bool done = false;
+    bool produce_done = false;
 public:
-    S_queue(int c) : capacity(c){}
+    S_queue(int Capacity) : capacity(Capacity){}
 
     void q_push(int item){
         unique_lock<mutex> ul(mtx);
         not_full.wait(ul, [this]{return q.size() < capacity;});
         q.push(item);
-        cout << "生产者" << this_thread::get_id() << "传入了一个数据，值为：" << item << endl;
+        cout << "生产者线程" << this_thread::get_id() << "生产了一个数据：" << item << endl;
         not_empty.notify_one();
     }
 
     bool q_pop(){
         unique_lock<mutex> ul(mtx);
-        not_empty.wait(ul, [this]{return !q.empty() || done;});
-        if(q.empty() && done){
+        not_empty.wait(ul, [this]{return produce_done || !q.empty();});
+        if(produce_done && q.empty()){
             return false;
         }
         int val = q.front();
         q.pop();
-        cout << "消费者" << this_thread::get_id() << "拿走了一个数据，值为" << val << endl;
+        cout << "消费者线程" << this_thread::get_id() << "消费了一个数据：" << val << endl;
         not_full.notify_one();
         return true;
     }
 
-    void produce_done(){
+    void ProduceDone(){
         unique_lock<mutex> ul(mtx);
-        done = true;
+        produce_done = true;
         not_empty.notify_all();
     }
 };
 
-void produce(S_queue& s_q, int start_id){
+void produce(S_queue& s_q, const int& start_id){
     for(int i = 1; i <= 10; i++){
         s_q.q_push(10*start_id + i);
     }
@@ -56,33 +56,32 @@ void produce(S_queue& s_q, int start_id){
 
 void consume(S_queue& s_q){
     while(true){
-        bool ref = s_q.q_pop();
-        if(!ref) break;
+        bool consume_done = !s_q.q_pop();
+        if(consume_done) break;
     }
 }
 
 int main(){
     auto start = chrono::steady_clock::now();
-    S_queue SQ(10);
-    vector<thread> producers, consumers;
+    S_queue s_q(10);
+    vector<thread> producer, consumer;
+    for(int i = 0; i < 2; i++){
+        producer.emplace_back(produce, ref(s_q), i);
+    }
+
+    for(int i = 0; i < 3; i++){
+        consumer.emplace_back(consume, ref(s_q));
+    }
 
     for(int i = 0; i < 2; i++){
-        producers.emplace_back(produce, ref(SQ), i);
+        producer[i].join();
     }
+    s_q.ProduceDone();
     for(int i = 0; i < 3; i++){
-        consumers.emplace_back(consume, ref(SQ));
+        consumer[i].join();
     }
-
-    for(auto& p : producers){
-        p.join();
-    }
-    SQ.produce_done();
-    for(auto& c : consumers){
-        c.join();
-    }
-
     auto end = chrono::steady_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "程序执行结束，耗时：" << duration.count() << "微秒。" << endl;
+    cout << "程序执行结束，耗费时间：" << duration.count() << "微秒！" << endl;
     return 0;
 }
